9adf7d9d38dfff35808e0e4ca64c9b2389866a26
CodeShft <nileceyazilim@gmail.com>
2024-03-24 18:42:53 +0300

    useref örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import { useState } from "react";
-import C1 from "./bilesenler/C1";
-import GlobalContext from "./contexts/GlobalContext";
+import { useRef, useState } from "react";
 
 function App() {
-  const [kullanici, kullaniciGuncelle] = useState("Orhan Gencebay");
+  const [sayac, sayacGuncelle] = useState(0)
+
+  const puan = useRef(0)
+
+  function puanGuncelle() {
+    puan.current += 10
+    alert(puan.current)
+  }
 
   return (
-    <GlobalContext.Provider value={kullanici}>
+    <div>
+      Puan: {puan.current}
+
       <div>
-        <h1>App</h1>
-        <C1 />
+        {sayac}
+        <button onClick={ ()=>{ sayacGuncelle(eskiDeger=>eskiDeger+5) } }>Sayaç Arttır</button>
       </div>
-    </GlobalContext.Provider>
+      <div>
+        <button onClick={puanGuncelle}>Ref Değeri Güncelle</button>
+      </div>
+    </div>
   );
 }
 
